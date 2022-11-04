00db55d057776108f472fc846c8dc6cbe3482fd1
Admin <sportislife635@gmail.com>
2022-11-05 00:43:18 +0300

    added Gtest lib

---
+++ test/Server/TestGTest.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Admin on 03.11.2022.
+//
+#include "gtest/gtest.h"
+
+TEST(TestGtest, test1) {
+//arrange
+//act
+//assert
+EXPECT_EQ (1,1);
+EXPECT_EQ (2,2);
+EXPECT_EQ (3,4);
+}
---
+++ test/Server/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Admin on 03.11.2022.
+//
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
